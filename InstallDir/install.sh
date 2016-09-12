dc02851da0c14984c889a5589fb2000978410e5d
josh <josh43@email.arizona.edu>
2016-09-12 14:06:28 -0700

    pushed via gitPA()

--- InstallDir/install.sh
+++ InstallDir/install.sh
@@ -5,6 +5,6 @@ sudo chown root:${USER} /usr/bin/jlGen
 sudo cp ../JLexx/LexLoader/LexRunner/jlrun /usr/bin/jlRun
 sudo chown root:${USER} /usr/bin/jlRun
 
-sudo cp -f -R ../RegularExpression/*  /usr/share/JRegexp
-sudo cp -f -R ../JLexx/*              /usr/share/JLexx
+sudo cp -f -R ../RegularExpression  /usr/share/JRegexp
+sudo cp -f -R ../JLexx              /usr/share/JLexx
 
