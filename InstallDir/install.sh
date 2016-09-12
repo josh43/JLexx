e054c8b299deda480bc0835926607c5e49623ead
josh <josh43@email.arizona.edu>
2016-09-12 14:02:55 -0700

    With installgit add -Agit add -A

--- InstallDir/install.sh
+++ InstallDir/install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-sudo cp ../jlgen /usr/bin/jlGen
+sudo cp ../JLexx/LexLoader/jlgen /usr/bin/jlGen
 sudo chown root:${USER} /usr/bin/jlGen
-sudo cp ../LexRunner/jlrun /usr/bin/jlRun
+sudo cp ../JLexx/LexLoader/LexRunner/jlrun /usr/bin/jlRun
 sudo chown root:${USER} /usr/bin/jlRun
 
 sudo cp -f -R ../RegularExpression/*  /usr/share/JRegexp
